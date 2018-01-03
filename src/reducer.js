4a5c7163d30c450516811af9a13ae1b87f45b6ac
Tristan Benier <tbenier@airware.com>
2018-01-03 17:24:42 +0100

    Separating actions and state mutations

--- src/reducer.js
+++ src/reducer.js
@@ -1,24 +1,10 @@
-const constants = require('./constants');
-const listStateMachines = require('./lib/actions/list-state-machines');
-const createStateMachine = require('./lib/actions/create-state-machine');
-const deleteStateMachine = require('./lib/actions/delete-state-machine');
-const describeStateMachine = require('./lib/actions/describe-state-machine');
-const listExecutions = require('./lib/actions/list-executions');
-const startExecution = require('./lib/actions/start-execution');
-const stopExecution = require('./lib/actions/stop-execution');
-const describeExecution = require('./lib/actions/describe-execution');
-const getExecutionHistory = require('./lib/actions/get-execution-history');
-const addHistoryEvent = require('./lib/actions/add-history-event');
+const { actions } = require('./constants');
 
 const initialState = {
   stateMachines: [],
   executions: [],
   responses: {},
 };
-
-// TODO
-// - add UpdateStateMachine new action
-// http://docs.aws.amazon.com/step-functions/latest/apireference/API_UpdateStateMachine.html
 
 function getErrorResponse(state, requestId, error) {
   return {
@@ -39,173 +25,208 @@ function getSuccessResponse(state, requestId, response) {
 }
 
 function reducer(state = initialState, action) {
-  const { actions } = constants;
-  const { params, type, requestId } = action;
-
+  const { type, requestId, result } = action;
   switch (type) {
+    // custom actions
     case actions.REMOVE_RESPONSE: {
       const stateCopy = Object.assign({}, state);
       delete stateCopy.responses[requestId];
       return stateCopy;
     }
-
-    // Not related to an existing resource
-    case actions.LIST_STATE_MACHINES: {
-      const { response } = listStateMachines(params, state.stateMachines);
-      return Object.assign({}, state, {
-        responses: getSuccessResponse(state, requestId, response),
-      });
-    }
-    case actions.CREATE_STATE_MACHINE: {
+    // actions related to state machines
+    case actions.CREATE_STATE_MACHINE:
       try {
-        const { stateMachine, response } = createStateMachine(params, state.stateMachines);
         return Object.assign({}, state, {
           stateMachines: [
             ...state.stateMachines,
-            stateMachine,
+            result.stateMachine,
           ],
-          responses: getSuccessResponse(state, requestId, response),
+          responses: getSuccessResponse(state, requestId, result.response),
         });
       } catch (e) {
         const responses = getErrorResponse(state, requestId, e);
         return Object.assign({}, state, { responses });
       }
-    }
-
-    // Related to one state machine
-    case actions.DELETE_STATE_MACHINE: {
+    case actions.LIST_STATE_MACHINES:
       try {
-        const { index, response } = deleteStateMachine(params, state.stateMachines);
-        const stateCopy = Object.assign({}, state, {
-          responses: getSuccessResponse(state, requestId, response),
+        return Object.assign({}, state, {
+          responses: getSuccessResponse(state, requestId, result.response),
         });
-        stateCopy.stateMachines.splice(index, 1);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.DESCRIBE_STATE_MACHINE:
+      try {
+        return Object.assign({}, state, {
+          responses: getSuccessResponse(state, requestId, result.response),
+        });
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.UPDATE_STATE_MACHINE:
+      // TODO
+      // http://docs.aws.amazon.com/step-functions/latest/apireference/API_UpdateStateMachine.html
+      try {
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.DELETE_STATE_MACHINE:
+      try {
+        const stateCopy = Object.assign({}, state, {
+          responses: getSuccessResponse(state, requestId, result.response),
+        });
+        stateCopy.stateMachines.splice(result.index, 1);
         return stateCopy;
       } catch (e) {
-        return Object.assign({}, state, {
-          responses: getErrorResponse(state, requestId, e),
-        });
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
       }
-    }
-    case actions.DESCRIBE_STATE_MACHINE: {
+    // actions related to activities
+    case actions.CREATE_ACTIVITY:
+      // TODO
       try {
-        const { response } = describeStateMachine(params, state.stateMachines);
-        return Object.assign({}, state, {
-          responses: getSuccessResponse(state, requestId, response),
-        });
+        return Object.assign({}, state);
       } catch (e) {
-        return Object.assign({}, state, {
-          responses: getErrorResponse(state, requestId, e),
-        });
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
       }
-    }
-    case actions.LIST_EXECUTIONS: {
+    case actions.LIST_ACTIVITIES:
+      // TODO
       try {
-        const { response } = listExecutions(params, state.stateMachines, state.executions);
-        return Object.assign({}, state, {
-          responses: getSuccessResponse(state, requestId, response),
-        });
+        return Object.assign({}, state);
       } catch (e) {
-        return Object.assign({}, state, {
-          responses: getErrorResponse(state, requestId, e),
-        });
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
       }
-    }
-    case actions.START_EXECUTION: {
+    case actions.DELETE_ACTIVITY:
+      // TODO
       try {
-        const { execution, response } = startExecution(
-          params,
-          state.stateMachines,
-          state.executions,
-        );
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.DESCRIBE_ACTIVITY:
+      // TODO
+      try {
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.GET_ACTIVITY_TASK:
+      // TODO
+      try {
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.SEND_TASK_FAILURE:
+      // TODO
+      try {
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.SEND_TASK_HEARTBEAT:
+      // TODO
+      try {
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.SEND_TASK_SUCCESS:
+      // TODO
+      try {
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    // actions related to executions
+    case actions.START_EXECUTION:
+      try {
         return Object.assign({}, state, {
           executions: [
             ...state.executions,
+            result.execution,
+          ],
+          responses: getSuccessResponse(state, requestId, result.response),
+        });
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.LIST_EXECUTIONS:
+      try {
+        return Object.assign({}, state, {
+          responses: getSuccessResponse(state, requestId, result.response),
+        });
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.ADD_HISTORY_EVENT:
+      try {
+        const stateCopy = Object.assign({}, state);
+        const execution = stateCopy.executions.find(e => e.executionArn === result.executionArn);
+        execution.events.push(result.event);
+        return Object.assign({}, stateCopy, {
+          executions: [
+            ...stateCopy.executions,
             execution,
           ],
-          responses: getSuccessResponse(state, requestId, response),
+          responses: getSuccessResponse(stateCopy, requestId),
         });
       } catch (e) {
-        return Object.assign({}, state, {
-          responses: getErrorResponse(state, requestId, e),
-        });
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
       }
-    }
-
-    // Related to one state machine execution
-    case actions.DESCRIBE_EXECUTION: {
+    case actions.DESCRIBE_EXECUTION:
       try {
-        const { response } = describeExecution(params, state.executions);
         return Object.assign({}, state, {
-          responses: getSuccessResponse(state, requestId, response),
+          responses: getSuccessResponse(state, requestId, result.response),
         });
       } catch (e) {
-        return Object.assign({}, state, {
-          responses: getErrorResponse(state, requestId, e),
-        });
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
       }
-    }
     case actions.GET_EXECUTION_HISTORY:
       try {
-        const { response } = getExecutionHistory(params, state.executions);
         return Object.assign({}, state, {
-          responses: getSuccessResponse(state, requestId, response),
+          responses: getSuccessResponse(state, requestId, result.response),
         });
       } catch (e) {
-        return Object.assign({}, state, {
-          responses: getErrorResponse(state, requestId, e),
-        });
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
+      }
+    case actions.UPDATE_EXECUTION:
+      // TODO
+      try {
+        return Object.assign({}, state);
+      } catch (e) {
+        const responses = getErrorResponse(state, requestId, e);
+        return Object.assign({}, state, { responses });
       }
     case actions.STOP_EXECUTION:
       try {
-        const newState = Object.assign({}, state);
-        const { response } = stopExecution(params, newState.executions);
-        return Object.assign({}, newState, {
-          responses: getSuccessResponse(state, requestId, response),
-        });
-      } catch (e) {
-        return Object.assign({}, state, {
-          responses: getErrorResponse(state, requestId, e),
-        });
-      }
-    case actions.CREATE_ACTIVITY:
-      return Object.assign({}, state);
-    case actions.LIST_ACTIVITIES:
-      return Object.assign({}, state);
-    case actions.ADD_HISTORY_EVENT: {
-      try {
-        const execution = state.executions.find(e => e.executionArn === params.executionArn);
-        const event = addHistoryEvent(params, execution);
-        execution.events.push(event);
-        return Object.assign({}, state, {
-          executions: [
-            ...state.executions,
-            execution,
-          ],
-          responses: getSuccessResponse(state, requestId),
+        const stateCopy = Object.assign({}, state);
+        stateCopy.executions[result.index] = result.execution;
+        return Object.assign({}, stateCopy, {
+          responses: getSuccessResponse(state, requestId, result.response),
         });
       } catch (e) {
         const responses = getErrorResponse(state, requestId, e);
         return Object.assign({}, state, { responses });
       }
-    }
-    case actions.UPDATE_EXECUTION: {
-      return Object.assign({}, state);
-    }
-
-    // Related to one execution activity
-    case actions.DELETE_ACTIVITY:
-      return Object.assign({}, state);
-    case actions.DESCRIBE_ACTIVITY:
-      return Object.assign({}, state);
-    case actions.GET_ACTIVITY_TASK:
-      return Object.assign({}, state);
-    case actions.SEND_TASK_FAILURE:
-      return Object.assign({}, state);
-    case actions.SEND_TASK_HEARTBEAT:
-      return Object.assign({}, state);
-    case actions.SEND_TASK_SUCCESS:
-      return Object.assign({}, state);
+    // default action
     default:
       return state;
   }
